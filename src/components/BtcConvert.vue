4a2c40f96b38130d6e2cf59466ad1e3521a9ec7b
Thomas Berghuis <thomas.berghuis@gmail.com>
2020-09-01 15:15:30 +1000

    .

--- src/components/BtcConvert.vue
+++ src/components/BtcConvert.vue
@@ -8,23 +8,13 @@
 </template>
 
 <script>
-import { convertToBtc } from "../services/convert.js";
-import Eth from "./Eth.vue";
 import Coin from "./Coin.vue";
 
 export default {
   name: "BtcConvert",
-  data() {
-    return { usdt: 0, btc: 0 };
-  },
-  methods: {
-    convertToBtc() {
-      console.log("usd", this.usdt);
-      convertToBtc(this.usdt);
-    }
-  },
+
   components: {
-    Eth, Coin
+    Coin
   }
 };
 </script>
--- src/subjects/btcValue.js
+++ src/subjects/btcValue.js
@@ -4,19 +4,12 @@ import { tradingPairPrice } from "../data/tradingPairPrice.js";
 
 export const btcValueSubject = new Subject();
 
-// toremove
-// window.btcValueSubject = btcValueSubject;
-
-// export function convertCoinValueToBtcValue(coinSymbol, coinValue) {
-//   console.log("convertCoinValueToBtcValue");
-// }
-
 convertCoinEventSubject.subscribe((coin) => {
   let btcValue;
   if (coin.coinSymbol == "BTC") {
     btcValue = coin.coinValue;
   } else if (coin.coinSymbol == "USDT") {
-    //TODO
+    btcValue = coin.coinValue / tradingPairPrice.BTCUSDT;
   } else if (tradingPairPrice[`${coin.coinSymbol}BTC`]) {
     btcValue = coin.coinValue * tradingPairPrice[`${coin.coinSymbol}BTC`];
   } else if (tradingPairPrice[`${coin.coinSymbol}USDT`]) {
