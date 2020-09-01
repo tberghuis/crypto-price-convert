4828ed123d82381b87f31ab0bddef8355057b4b2
Thomas Berghuis <thomas.berghuis@gmail.com>
2020-09-01 11:27:01 +1000

    .

--- src/services/convert.js
+++ src/services/convert.js
@@ -1,8 +1,20 @@
 import axios from "axios";
+// import got from "got";
 // const got = require("got");
 
 function convertToBtc(usdt) {
   console.log("convert to btc", usdt);
+
+  // (async () => {
+  //   try {
+  //     const response = await got("https://sindresorhus.com");
+  //     console.log(response.body);
+  //     //=> '<!doctype html> ...'
+  //   } catch (error) {
+  //     console.log(error.response.body);
+  //     //=> 'Internal server error ...'
+  //   }
+  // })();
 
   axios
     .get("https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT")
