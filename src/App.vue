e0c617a3fde9ae4cf261e40b12cc729aefc9009c
Thomas Berghuis <thomas.berghuis@gmail.com>
2020-09-03 15:09:49 +1000

    .

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
+    <ArrayTest></ArrayTest>
     <BtcConvert></BtcConvert>
   </div>
 </template>
 
 <script>
 import BtcConvert from "./components/BtcConvert.vue";
+import ArrayTest from "./components/ArrayTest.vue";
 
 export default {
   name: "App",
   components: {
-    BtcConvert
+    BtcConvert,
+    ArrayTest
   }
 };
 </script>
---
+++ src/components/ArrayTest.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div>array test</div>
+    <div>{{arrRef}}</div>
+    <button v-on:click="buttonClick">Add 1</button>
+  </div>
+</template>
+
+<script>
+import { ref, reactive } from "vue";
+
+const arrRef = ref([0, 1]);
+
+const buttonClick = () => {
+  arrRef.value.push(2);
+};
+
+export default {
+  setup() {
+    // expose to template
+    return {
+      arrRef,
+      buttonClick
+    };
+  }
+};
+</script>
+
+<style>
+</style>
